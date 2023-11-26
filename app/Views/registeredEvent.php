b6e891f7447d2882a78678bd15e43c924139c8ba
Guilherme <guilherme.souza16@alunos.sc.senac.br>
2023-11-26 18:41:08 -0300

    fechou

--- app/Views/registeredEvent.php
+++ app/Views/registeredEvent.php
@@ -13,9 +13,40 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
-<div class="dashboard">
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="dashboard">V&G Events</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="dashboard">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="createEvent">Cadastrar Eventos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="registeredEvent">Eventos Cadastrados</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="showVoucher">Vouchers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout">logout</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true"><?php session('user_name')?></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <div class="dashboard">
 
-    <body>
+
 
         <h1>Eventos Cadastrados</h1>
 
@@ -50,7 +81,8 @@
             }
         }
         ?>
-    </body>
-</div>
+    </div>
+</body>
+
 
 </html>
